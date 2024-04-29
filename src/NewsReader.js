5bb881314710f3801c261ddc18cde0afbdebc876
Sritharan_Srinivasan <srisritharan.s@gmail.com>
2024-04-29 19:49:41 -0400

    Login restricted features added

--- src/NewsReader.js
+++ src/NewsReader.js
@@ -3,7 +3,7 @@ import { SavedQueries } from './SavedQueries';
 import { QueryForm } from './QueryForm';
 import { Articles } from './Articles';
 import { useState, useEffect } from 'react';
-import { exampleQuery ,exampleData } from './data';
+import { exampleQuery, exampleData } from './data';
 import { LoginForm } from './LoginForm';
 
 export function NewsReader() {
@@ -15,7 +15,7 @@ export function NewsReader() {
   const [currentUser, setCurrentUser] = useState(null);
   const [credentials, setCredentials] = useState({ user: "", password: "" });
 
-  const urlNews="/news"
+  const urlNews = "/news"
   const urlQueries = "/queries"
   const urlUsersAuth = "/users/authenticate";
 
@@ -24,13 +24,13 @@ export function NewsReader() {
     getNews(query);
   }, [query])
 
-  useEffect(() => {getQueryList();}, [])
+  useEffect(() => { getQueryList(); }, [])
 
   async function login() {
-    if(currentUser !== null) {
+    if (currentUser !== null) {
       setCurrentUser(null)
     } else {
-      
+
       try {
         const response = await fetch(urlUsersAuth, {
           method: 'POST',
@@ -51,7 +51,7 @@ export function NewsReader() {
     }
 
   }
-  
+
 
   async function getQueryList() {
     try {
@@ -61,10 +61,10 @@ export function NewsReader() {
       }
     } catch (error) {
       console.error('Error fetching news:', error);
-    } 
+    }
   }
 
-  async function saveQueryList(savedQueries) { 
+  async function saveQueryList(savedQueries) {
     try {
       const response = await fetch(urlQueries, {
         method: 'POST',
@@ -73,22 +73,41 @@ export function NewsReader() {
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
-      console.log("savedQueries array has been persisted:"); 
+      console.log("savedQueries array has been persisted:");
     } catch (error) {
       console.error('Error fetching news:', error);
     }
   }
 
 
-  function onSavedQuerySelect(selectedQuery) { 
+  function onSavedQuerySelect(selectedQuery) {
     setQueryFormObject(selectedQuery); setQuery(selectedQuery);
   }
-  
-  function onFormSubmit(queryObject) {
-    let newSavedQueries = []; newSavedQueries.push(queryObject); for (let query of savedQueries) {
-      if (query.queryName !== queryObject.queryName) { newSavedQueries.push(query);} 
+
+  function currentUserMatches(user) {
+    if (currentUser) {
+      if (currentUser.user) {
+        if (currentUser.user === user) {
+          return true;
+        }
+      }
     }
-    console.log(JSON.stringify(newSavedQueries)); 
+    return false;
+  }
+
+  function onFormSubmit(queryObject) {
+    if (currentUser === null){
+      alert("Log in if you want to create new queries!")
+      return;
+      }
+    if (savedQueries.length >= 3 && currentUserMatches("guest")) {
+      alert("guest users cannot submit new queries once saved query count is 3 or greater!")
+      return;
+      }
+      let newSavedQueries = []; newSavedQueries.push(queryObject); for (let query of savedQueries) {
+      if (query.queryName !== queryObject.queryName) { newSavedQueries.push(query); }
+    }
+    console.log(JSON.stringify(newSavedQueries));
     saveQueryList(newSavedQueries);
     setSavedQueries(newSavedQueries);
     setQuery(queryObject);
@@ -96,23 +115,23 @@ export function NewsReader() {
 
   async function getNews(queryObject) {
     if (queryObject.q) {
-        // setData(exampleData);
+      // setData(exampleData);
 
-        try {
-          const response = await fetch(urlNews, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(queryObject),
-          });
-    
-          if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-          }
-          const data = await response.json();
-          setData(data);
-        } catch (error) {
-          console.error('Error fetching news:', error);
+      try {
+        const response = await fetch(urlNews, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(queryObject),
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
         }
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error('Error fetching news:', error);
+      }
 
     } else {
       setData({});
@@ -122,26 +141,27 @@ export function NewsReader() {
   return (
     <div>
       <LoginForm login={login}
-           credentials={credentials}
-           currentUser={currentUser}
-           setCredentials={setCredentials}  />
+        credentials={credentials}
+        currentUser={currentUser}
+        setCredentials={setCredentials} />
       <div >
         <section className="parent" >
           <div className="box">
             <span className='title'>Query Form</span>
             <QueryForm
+              currentUser={currentUser}
               setFormObject={setQueryFormObject}
               formObject={queryFormObject}
               submitToParent={onFormSubmit} />
           </div>
 
           <div className="box">
-            <span className='title'>Saved Queries</span> 
+            <span className='title'>Saved Queries</span>
             <SavedQueries
               savedQueries={savedQueries}
-              selectedQueryName={query.queryName} 
-              onQuerySelect={onSavedQuerySelect} 
-            /> 
+              selectedQueryName={query.queryName}
+              onQuerySelect={onSavedQuerySelect}
+            />
           </div>
 
           <div className="box">
